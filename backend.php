f8220d10213655871a6e655ec50c2fe81ead4cef
Yasin <65618247+suphiyasin@users.noreply.github.com>
2022-06-08 20:48:02 +0300

    Update backend.php

--- backend.php
+++ backend.php
@@ -1,5 +1,5 @@
 <?php
 include("api.php");
 $use = new tele();
-$use->cookie = "stel_web_auth=https%3A%2F%2Fweb.telegram.org%2Fk%2F'";             //ENTER A COOKIE
+$use->cookie = "stel_web_auth=https%3A%2F%2Fweb.telegram.org%2Fk%2F'";             //If you need cookies in the future.But now working...
 $use->tgfind();
